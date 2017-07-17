5a69250e987017473f06d92ab51b6c572250e785
emberrap <emberrap@students.zhaw.ch>
2017-07-17 11:10:47 +0200

    Halting the js YT downloader because I couldn't find a way to circumvent same-origin policy and there's virtually no TCP socket support for JS.

--- resources/js/ytdownloader.js
+++ resources/js/ytdownloader.js
@@ -44,18 +44,96 @@ $(function() {
     // $.get("https://youtube.com/watch?v=" + videoId).done(function (data) {
     //   console.log(data);
     // });
-    $.ajax({
-      url: "https://youtube.com/watch?v=" + videoId,
-      method: "GET",
-      crossDomain: true,
-      dataType: 'html',
-      success: function() {
-        alert("Success");
-      },
-      error: function() {
-        alert('Failed!');
-      },
-    })
+    // $.get("https://www.googleapis.com/youtube/v3/videos" +
+    //       "?id=7lCDEYXw3mM" +
+    //       "&key=AIzaSyA-dlBUjVQeuc4a6ZN4RkNUYDFddrVLxrA" +
+    //       "&part=snippet,contentDetails,statistics,status",
+    //     function(data) {
+    //       alert(data);
+    //     });
+
+    // var tcp = TCPSocket.open("https://www.youtube.com/watch?v=" + videoId, 80);
+    var data = "GET https://www.youtube.com/watch?v=" + videoId + " HTTP/1.1\n";
+    data += "Host: www.youtube.com\n";
+    data += "User-Agent: Mozilla/4.0 (compatible; MSIE5.01; Windows NT)\n";
+    data += "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8\n";
+    data += "Accept-Language: en-US,en;q=0.5\n";
+    data += "Accept-Encoding: gzip, deflate, br\n";
+    data += "Connection: Keep-Alive";
+    // tcp.send(data);
+
+    // var promise = navigator.tcpPermission.requestPermission({remoteAddress:"127.0.0.1", remotePort:6789});
+
+
+    // var host = "https://www.youtube.com/watch?v=" + videoId;
+    // var port = 80;
+    // var socket = new jSocket();
+    // socket.onReady = function(){
+    //         socket.connect(host, port);
+    // }
+    // socket.onConnect = function(success, msg){
+    //         if(success){
+    //                 socket.write(data);
+    //         }else{
+    //                 alert('Connection to the server could not be estabilished: ' + msg);
+    //         }
+    // }
+    // socket.onData = function(data){
+    //         alert('Received from socket: '+data);
+    // }
+
+
+    // var myHeaders = new Headers();
+    // myHeaders.append('Content-Type', 'text/html');
+    // myHeaders.append('Origin', 'https://www.youtube.com/');
+    // myHeaders.append('User-Agent', 'Der Geraet');
+    // var myInit = { method: 'GET',
+    //                headers: myHeaders,
+    //                cache: 'default' };
+    // var req = new Request("https://www.youtube.com/watch?v=" + videoId, myInit);
+    // var myRequest = new Request("https://www.youtube.com/watch?v=" + videoId, {method: 'POST', body: '{"foo":"bar"}'});
+    // myRequest.origin = "";
+    // myRequest.referrer = "no-referrer";
+    // myRequest.use-CORS-preflight
+    // fetch(myRequest).then(function(response) {
+    //   alert(response);
+    // });
+
+    // req.open("get", "https://youtube.com/watch?v=" + videoId, true);
+    // req.setRequestHeader('Content-type','text/html');
+    // req.setRequestHeader('Origin', 'https://youtube.com/');
+    // req.setRequestHeader('ABC', 'ES TUET NÜME WEH!');
+    // req.setRequestHeader('User-Agent', 'Der Geraet');
+    // req.onreadystatechange = function () {
+    //   alert(req.readyState + " - " + req.status);
+    // };
+    // req.send();
+    // return;
+    //
+    // $.ajax({
+    //   type: "GET",
+    //   beforeSend: function(jqXHR, settings) {
+    //     jqXHR.setRequestHeader("Origin", "https://youtube.com/");
+    //     jqXHR.setRequestHeader("Referer", "https://youtube.com/");
+    //   },
+    //   url: "https://youtube.com/watch?v=" + videoId,
+    //   success: function(msg) {
+    //     alert(msg);
+    //   }
+    // });
+    // $.ajax({
+    //   url: "https://youtube.com/watch?v=" + videoId,
+    //   method: "GET",
+    //   crossDomain: true,
+    //   dataType: 'html',
+    //   headers: {headr};
+    //   success: function() {
+    //     alert("Success");
+    //   },
+    //   error: function() {
+    //     alert('Failed!');
+    //   },
+    // })
   })
 })
 
